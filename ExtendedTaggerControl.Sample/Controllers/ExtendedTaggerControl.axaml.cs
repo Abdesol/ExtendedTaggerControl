a6a6f8a28cd1715cee8caa2953f2d2f650c55707
Abdella Solomon <abdesoltak@gmail.com>
2022-02-10 22:41:22 +0300

    Added the ExtendedTaggerControl and GetTag Command to see if the tag contains a data.

---
+++ ExtendedTaggerControl.Sample/Controllers/ExtendedTaggerControl.axaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+
+namespace ExtendedTaggerControl.Sample.Controllers
+{
+    public class ExtendedTaggerControl : UserControl
+    {
+        static ExtendedTaggerControl()
+        {
+            WatermarkProperty.Changed.Subscribe(WatermarkPropertyChanged);
+            TagsProperty.Changed.Subscribe(TagsPropertyChanged);
+        }
+        
+        private TextBox _textBox;
+        private ItemsControl _tagsList;
+        public ExtendedTaggerControl()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            this.GetControl("TagAdder", out _textBox);
+            this.GetControl("TagsList", out _tagsList);
+            
+            _textBox.AddHandler(KeyDownEvent, (sender, args) =>
+            {
+                if (args.Key == Key.Enter)
+                {
+                    if(string.IsNullOrEmpty(_textBox.Text)) return;
+                    ((DataContext as ExtendedTaggerControlViewModel)!).AddTag(_textBox.Text);
+                    _textBox.Clear();
+                }
+            });
+
+            ((DataContext as ExtendedTaggerControlViewModel)!).Tags.CollectionChanged += (sender, args) =>
+            {
+                Tags = ((DataContext as ExtendedTaggerControlViewModel)!).Tags;
+            };
+        }
+        
+        public new static readonly StyledProperty<string> WatermarkProperty =
+            AvaloniaProperty.Register<ExtendedTaggerControl, string>(nameof(Watermark));
+
+        public new string Watermark
+        {
+            get => GetValue(WatermarkProperty);
+            set => SetValue(WatermarkProperty, value);
+        }
+
+        private static void WatermarkPropertyChanged(AvaloniaPropertyChangedEventArgs<string> e)
+        {
+            if (e.Sender is ExtendedTaggerControl ctrl)
+            {
+                ctrl._textBox.Watermark = e.NewValue.Value;
+            }
+        }
+
+        public new static readonly StyledProperty<IEnumerable> TagsProperty =
+            AvaloniaProperty.Register<ExtendedTaggerControl, IEnumerable>(
+                nameof(Tags),
+                defaultBindingMode: BindingMode.TwoWay);
+        
+        public IEnumerable Tags
+        {
+            get => GetValue(TagsProperty);
+            set => SetValue(TagsProperty, value);
+        }
+        
+        private static void TagsPropertyChanged(AvaloniaPropertyChangedEventArgs<IEnumerable> e)
+        {
+            if (e.Sender is ExtendedTaggerControl ctrl)
+            {
+                var newTags = e.NewValue.Value as ObservableCollection<string>;
+                ((ctrl.DataContext as ExtendedTaggerControlViewModel)!).Tags =
+                    newTags;
+            }
+        }
+    }
+    
+    public static class Extensions
+    {
+        public static void GetControl<T>(this Control control, string name, out T element) where T : Control
+        {
+            element = control.FindControl<T>(name);
+        }
+    }
+}
--- ExtendedTaggerControl.Sample/ViewModels/MainWindowViewModel.cs
+++ ExtendedTaggerControl.Sample/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
+using ReactiveUI;
 
 namespace ExtendedTaggerControl.Sample.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            Tags = new()
+            {
+                "This", "is", "Sample"
+            };
+        }
+
+        private ObservableCollection<string> _tags;
+        public ObservableCollection<string> Tags
+        {
+            get => _tags;
+            set => this.RaiseAndSetIfChanged(ref _tags, value);
+        }
+
+        public async void GetCommand()
+        {
+            Debug.WriteLine($"Tags length is {Tags.Count}");
+        }
     }
 }
